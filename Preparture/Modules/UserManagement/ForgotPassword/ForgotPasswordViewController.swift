978d970cb8526d228eef1a3d82e2b42cb77b79ee
nimmykdas <nimmypk1990@gmail.com>
2018-07-01 22:51:52 +0530

    forgot password ui completed

--- Preparture/Modules/UserManagement/ForgotPassword/ForgotPasswordViewController.swift
+++ Preparture/Modules/UserManagement/ForgotPassword/ForgotPasswordViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ForgotPasswordViewController: BaseViewController {
 
+    @IBOutlet weak var tfEmail: UITextField!
+    
     override func initView() {
         super.initView()
         
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - UIView Action Methods
+    
+    @IBAction func actionContinue(_ sender: Any) {
+        
+    }
+    
+    @IBAction func actionBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK : -> ------ UITextField Delegates ------
+
+extension ForgotPasswordViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == tfEmail {
+            tfEmail.resignFirstResponder()
+        }
+        return true
+    }
 }
