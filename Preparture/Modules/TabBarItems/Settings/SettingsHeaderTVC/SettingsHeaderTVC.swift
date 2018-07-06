b54171de3e02704a7f665d0225d9cfca19421b4f
Bibin Mathew <bibin.m@codelynks.com>
2018-07-06 06:27:48 +0530

    Settings view created.

---
+++ Preparture/Modules/TabBarItems/Settings/SettingsHeaderTVC/SettingsHeaderTVC.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsHeaderTVC.swift
+//  Preparture
+//
+//  Created by Bibin Mathew on 7/6/18.
+//  Copyright © 2018 praveen raj. All rights reserved.
+//
+
+import UIKit
+
+class SettingsHeaderTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
